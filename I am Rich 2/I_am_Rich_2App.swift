a42650d11f926507aeeb3924d47ef92d99009101
Danyil Marchuk <zemero58@gmail.com>
2023-01-16 12:59:04 +0100

    Initial Commit

---
+++ I am Rich 2/I_am_Rich_2App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich_2App.swift
+//  I am Rich 2
+//
+//  Created by Данік on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
